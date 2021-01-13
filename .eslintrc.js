ca8a8f4aac0b60c1ac178764d9dd799b41260b3b
이충희 <dreamjr@fixelsoft.com>
2021-01-13 14:11:08 +0900

    eslint rule 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,21 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   env: {
     browser: true,
+    node: true
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
+  extends: ["plugin:vue/essential"],
+  plugins: ["vue"],
+  parserOptions: {
+    parser: "babel-eslint",
+    sourceType: "module"
+  },
   rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    "curly": "error",
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "semi": ["error"]
+  },
+  root: true
 }
